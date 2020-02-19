9c1e305c203231cb2b39cce1d07bad111801b98c
Angelina Panteley <angelina@localhost.localdomain>
2020-02-19 22:22:32 +0300

    Add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,31 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if(pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        res.end('400 Not Supported');
+        return;
+      }
 
+      fs.access(filepath, fs.F_OK, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('404 Not Found');
+          return;
+        }
+
+        fs.unlink(filepath, (err) => {
+          if(err) {
+            res.statusCode = 500;
+            res.end('Internal server error');
+            return;
+          }
+
+          res.statusCode = 200;
+          res.end();
+        });
+      });
+  
       break;
 
     default:
