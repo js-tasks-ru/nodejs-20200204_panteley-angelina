29a011edf69f00fc596f07dbadf9490b747dd2e9
Angelina Panteley <angelina@localhost.localdomain>
2020-03-01 20:26:33 +0000

    Added solution for 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,50 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+function getProduct(elem) {
+  return {
+    id: elem._id,
+    category: elem.category,
+    description: elem.description,
+    images: elem.images,
+    price: elem.price,
+    subcategory: elem.subcategory,
+    title: elem.title,
+  }
+}
+
+function getProductList(products) {
+  return products.map((elem) => getProduct(elem));
+}
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  if(ctx.request.query.subcategory) {
+    await Product.find({subcategory: ctx.request.query.subcategory}, function(err, products) {
+      ctx.body = {products: getProductList(products)};
+    })
+  } else {
+    await next();
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  await Product.find({}, function(err, products) {
+    ctx.body = {products: getProductList(products)};
+  });
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const id = ctx.request.URL.pathname.split('/')[3];
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.response.status = 400;
+  } else {
+    await Product.find({_id: id}, function(err, products) {
+      if(err || !products.length) {
+        ctx.response.status = 404;
+      } else {
+        ctx.body = {product: getProduct(products[0])};
+      }
+    });
+  }
 };
 
